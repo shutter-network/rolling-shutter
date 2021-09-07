56324810f408a44d3bd20349a353e57b936aadfc
Jannik Luhn <jannik@brainbot.com>
2021-09-07 11:49:38 +0200

    Add function to check if db is empty

--- rolling-shutter/shdb/db.go
+++ rolling-shutter/shdb/db.go
@@ -15,6 +15,16 @@ const tableNamesQuery = `
 	AND table_schema NOT LIKE 'pg_toast%'
 `
 
+// dbNotEmptyQuery returns one row if there is at least one user created table in the database,
+// otherwise no row.
+const dbNotEmptyQuery = `
+	SELECT 1
+	FROM information_schema.tables
+	WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
+	AND table_schema NOT LIKE 'pg_toast%'
+	LIMIT 1;
+`
+
 // ValidateKeyperDB checks that all expected tables exist in the database. If not, it returns an
 // error.
 func ValidateKeyperDB(ctx context.Context, dbpool *pgxpool.Pool) error {
@@ -63,4 +73,22 @@ func validateDB(ctx context.Context, dbpool *pgxpool.Pool, requiredTables []stri
 		return errors.New("database misses one or more required table")
 	}
 	return nil
+}
+
+// IsEmpty checks if the db contains no non-default tables.
+func IsEmpty(ctx context.Context, dbpool *pgxpool.Pool) (bool, error) {
+	rows, err := dbpool.Query(ctx, dbNotEmptyQuery)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to ask db if it is empty")
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		return false, nil // got row, no error
+	}
+	err = rows.Err()
+	if err != nil {
+		return true, errors.Wrap(err, "failed to get row from query") // no row, but error
+	}
+	return true, nil // no row, no error
 }
