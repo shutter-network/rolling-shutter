67ad69dc7819dc961edad1cb4cf06685a23ebf1a
Jannik Luhn <jannik@brainbot.com>
2021-09-07 13:35:41 +0200

    Add initialize decryptor db subcommand

--- rolling-shutter/shdb/db.go
+++ rolling-shutter/shdb/db.go
@@ -15,15 +15,33 @@ const tableNamesQuery = `
 	AND table_schema NOT LIKE 'pg_toast%'
 `
 
-// dbNotEmptyQuery returns one row if there is at least one user created table in the database,
-// otherwise no row.
-const dbNotEmptyQuery = `
-	SELECT 1
-	FROM information_schema.tables
-	WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
-	AND table_schema NOT LIKE 'pg_toast%'
-	LIMIT 1;
+// createDecryptorTables creates the tables for the decryptor db.
+const createDecryptorTables = `
+	CREATE TABLE IF NOT EXISTS cipher_batch (
+		round_id bigint PRIMARY KEY,
+		data bytea
+	);
+	CREATE TABLE IF NOT EXISTS decryption_key (
+		round_id bigint PRIMARY KEY,
+		key bytea
+	);
+	CREATE TABLE IF NOT EXISTS decryption_signature (
+		round_id bigint,
+		signed_hash bytea,
+		signer_index bigint,
+		signature bytea,
+		PRIMARY KEY (round_id, signer_index)
+	);
 `
+
+// InitDecryptorDB initializes the database of the decryptor. It is assumed that the db is empty.
+func InitDecryptorDB(ctx context.Context, dbpool *pgxpool.Pool) error {
+	_, err := dbpool.Exec(ctx, createDecryptorTables)
+	if err != nil {
+		return errors.Wrap(err, "failed to create decryptor tables")
+	}
+	return nil
+}
 
 // ValidateKeyperDB checks that all expected tables exist in the database. If not, it returns an
 // error.
@@ -73,22 +91,4 @@ func validateDB(ctx context.Context, dbpool *pgxpool.Pool, requiredTables []stri
 		return errors.New("database misses one or more required table")
 	}
 	return nil
-}
-
-// IsEmpty checks if the db contains no non-default tables.
-func IsEmpty(ctx context.Context, dbpool *pgxpool.Pool) (bool, error) {
-	rows, err := dbpool.Query(ctx, dbNotEmptyQuery)
-	if err != nil {
-		return false, errors.Wrap(err, "failed to ask db if it is empty")
-	}
-	defer rows.Close()
-
-	if rows.Next() {
-		return false, nil // got row, no error
-	}
-	err = rows.Err()
-	if err != nil {
-		return true, errors.Wrap(err, "failed to get row from query") // no row, but error
-	}
-	return true, nil // no row, no error
 }
