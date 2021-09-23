950df0c100d5be7860bc407e4fbbe6cfa52776c4
Ralf Schmitt <ralf@brainbot.com>
2021-09-23 12:17:38 +0200

    Remove the "if not exists" clauses from keyper table creation
    These are not needed, since we now track the schema version and use a
    transaction for the schema creation.

--- rolling-shutter/keyper/kprdb/schema.sql
+++ rolling-shutter/keyper/kprdb/schema.sql
@@ -2,22 +2,21 @@
 -- Please change the version above if you make incompatible changes to
 -- the schema. We'll use this to check we're using the right schema.
 
-CREATE SCHEMA IF NOT EXISTS keyper;
-CREATE TABLE IF NOT EXISTS keyper.decryption_trigger (
+CREATE SCHEMA keyper;
+CREATE TABLE keyper.decryption_trigger (
        epoch_id bytea PRIMARY KEY
 );
-CREATE TABLE IF NOT EXISTS keyper.decryption_key_share (
+CREATE TABLE keyper.decryption_key_share (
        epoch_id bytea,
        keyper_index bigint,
        decryption_key_share bytea,
        PRIMARY KEY (epoch_id, keyper_index)
 );
-CREATE TABLE IF NOT EXISTS keyper.decryption_key (
+CREATE TABLE keyper.decryption_key (
        epoch_id bytea PRIMARY KEY,
        keyper_index bigint,
        decryption_key bytea
 );
-
 CREATE TABLE keyper.meta_inf(
        key text PRIMARY KEY,
        value text NOT NULL
