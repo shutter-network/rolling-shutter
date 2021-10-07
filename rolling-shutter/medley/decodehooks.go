a59ca7399c1403ab6443e608f441c183e8fe908d
Jannik Luhn <jannik@brainbot.com>
2021-10-07 20:27:55 +0200

    Move decodehooks to medley

--- rolling-shutter/medley/decodehooks.go
+++ rolling-shutter/medley/decodehooks.go
@@ -1,11 +1,18 @@
 package medley
 
 import (
+	"crypto/ecdsa"
+	"crypto/ed25519"
 	"encoding/hex"
+	"fmt"
 	"reflect"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/ecies"
 	p2pcrypto "github.com/libp2p/go-libp2p-core/crypto"
 	multiaddr "github.com/multiformats/go-multiaddr"
+	"github.com/pkg/errors"
 
 	"github.com/shutter-network/shutter/shlib/shcrypto/shbls"
 )
@@ -81,4 +88,50 @@ func BLSPublicKeyHook(f reflect.Type, t reflect.Type, data interface{}) (interfa
 		return nil, err
 	}
 	return key, nil
+}
+
+func StringToEd25519PrivateKey(f reflect.Type, t reflect.Type, data interface{}) (interface{}, error) {
+	if f.Kind() != reflect.String || t != reflect.TypeOf(ed25519.PrivateKey{}) {
+		return data, nil
+	}
+	seed, err := hex.DecodeString(data.(string))
+	if err != nil {
+		return nil, err
+	}
+	if len(seed) != ed25519.SeedSize {
+		return nil, errors.Errorf("invalid seed length %d (must be %d)", len(seed), ed25519.SeedSize)
+	}
+	return ed25519.NewKeyFromSeed(seed), nil
+}
+
+func StringToEcdsaPrivateKey(f reflect.Type, t reflect.Type, data interface{}) (interface{}, error) {
+	if f.Kind() != reflect.String || t != reflect.TypeOf(&ecdsa.PrivateKey{}) {
+		return data, nil
+	}
+	return crypto.HexToECDSA(data.(string))
+}
+
+func StringToEciesPrivateKey(f reflect.Type, t reflect.Type, data interface{}) (interface{}, error) {
+	if f.Kind() != reflect.String || t != reflect.TypeOf(&ecies.PrivateKey{}) {
+		return data, nil
+	}
+	encryptionKeyECDSA, err := crypto.HexToECDSA(data.(string))
+	if err != nil {
+		return nil, err
+	}
+
+	return ecies.ImportECDSA(encryptionKeyECDSA), nil
+}
+
+func StringToAddress(f reflect.Type, t reflect.Type, data interface{}) (interface{}, error) {
+	if f.Kind() != reflect.String || t != reflect.TypeOf(common.Address{}) {
+		return data, nil
+	}
+
+	ds := data.(string)
+	addr := common.HexToAddress(ds)
+	if addr.Hex() != ds {
+		return nil, fmt.Errorf("not a checksummed address: %s", ds)
+	}
+	return addr, nil
 }
