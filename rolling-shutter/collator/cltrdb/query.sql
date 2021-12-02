7ee54a27c2ec9f654b3c2bfa431943aa84403ac5
cducrest <come.ducrest@gmail.com>
2021-12-02 19:58:57 +0100

    Use last batch epochID instead of trigger epochID for next batch
    The collator would try to store two batches with the same epochID
    when killed and restarted in between the time where it stores a
    batch and a decryption trigger when using the epochID from a trigger
    see https://github.com/shutter-network/rolling-shutter/issues/138

--- rolling-shutter/collator/cltrdb/query.sql
+++ rolling-shutter/collator/cltrdb/query.sql
@@ -10,14 +10,14 @@ INSERT INTO collator.decryption_trigger (epoch_id, batch_hash) VALUES ($1, $2);
 -- name: GetTrigger :one
 SELECT * FROM collator.decryption_trigger WHERE epoch_id = $1;
 
--- name: GetLastTrigger :one
-SELECT * FROM collator.decryption_trigger ORDER BY epoch_id DESC LIMIT 1;
-
 -- name: InsertBatch :exec
 INSERT INTO collator.cipher_batch (epoch_id, transactions) VALUES ($1, $2);
 
 -- name: GetBatch :one
 SELECT * FROM collator.cipher_batch WHERE epoch_id = $1;
+
+-- name: GetLastBatchEpochID :one
+SELECT epoch_id FROM collator.cipher_batch ORDER BY epoch_id DESC LIMIT 1;
 
 -- name: InsertTx :exec
 INSERT INTO collator.transaction (tx_id, epoch_id, encrypted_tx)VALUES ($1, $2, $3);
