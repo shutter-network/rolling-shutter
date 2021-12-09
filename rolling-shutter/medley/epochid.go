2a311e65d5bd8d4c43c7fae1bcd39c811cc9a458
Ralf Schmitt <ralf@brainbot.com>
2021-12-09 11:51:41 +0100

    Simplify medley.epochid
    Use more bit arithmetic where possible. Use math.MaxUint32 instead of declaring
    the constant in our code.
    
    I do think we should eventually pass/return the block number and sequence number
    as 32 bit integers, in order to get rid of the error handling.

--- rolling-shutter/medley/epochid.go
+++ rolling-shutter/medley/epochid.go
@@ -1,21 +1,27 @@
 package medley
 
-import "github.com/pkg/errors"
+import (
+	"math"
+
+	"github.com/pkg/errors"
+)
 
 // ActivationBlockNumberFromEpochID extracts the activation block number from an epoch id.
 func ActivationBlockNumberFromEpochID(epochID uint64) int64 {
-	return int64(epochID >> (8 * 4)) // take first 4 bytes
+	return int64(epochID >> 32) // take first 4 bytes
 }
 
 // SequenceNumberFromEpochID extracts the sequence number from an epoch id.
 func SequenceNumberFromEpochID(epochID uint64) int64 {
-	return int64(epochID << (8 * 4) >> (8 * 4)) // take last 4 bytes
+	return int64(epochID & math.MaxUint32) // take last 4 bytes
 }
 
 func EncodeEpochID(seq uint64, blk uint64) (uint64, error) {
-	maxUint32 := uint64(^uint32(0))
-	if seq > maxUint32 || blk > maxUint32 {
-		return 0, errors.Errorf("cannot fit block number %d and sequence number %d into an epochID", blk, seq)
+	if seq>>32 != 0 {
+		return 0, errors.Errorf("sequence number %d out of bounds", seq)
 	}
-	return (blk << (8 * 4)) + seq, nil
+	if blk>>32 != 0 {
+		return 0, errors.Errorf("block number %d out of bounds", blk)
+	}
+	return blk<<32 | seq, nil
 }
