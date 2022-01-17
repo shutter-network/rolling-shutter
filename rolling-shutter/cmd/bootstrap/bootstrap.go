ba3e4980cd94ebf5c78c75dd714c168f70c1d55f
Ralf Schmitt <ralf@brainbot.com>
2022-01-17 13:38:36 +0100

    Read values for bootstrapping from contracts
    This makes the keypers use the right activation block number for the first eon.

--- rolling-shutter/cmd/bootstrap/bootstrap.go
+++ rolling-shutter/cmd/bootstrap/bootstrap.go
@@ -3,19 +3,24 @@ package bootstrap
 import (
 	"context"
 	"log"
+	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/tendermint/tendermint/rpc/client/http"
 
+	"github.com/shutter-network/shutter/shuttermint/contract"
+	"github.com/shutter-network/shutter/shuttermint/contract/deployment"
 	"github.com/shutter-network/shutter/shuttermint/keyper/fx"
 	"github.com/shutter-network/shutter/shuttermint/shmsg"
 )
 
 var bootstrapFlags struct {
 	ShuttermintURL   string
+	EthereumURL      string
+	DeploymentDir    string
 	BatchConfigIndex int
 	SigningKey       string
 	Keypers          []string
@@ -35,6 +40,22 @@ chain's genesis config.`,
 			return bootstrap()
 		},
 	}
+
+	cmd.PersistentFlags().StringVarP(
+		&bootstrapFlags.EthereumURL,
+		"ethereum-url",
+		"",
+		"http://localhost:8545",
+		"Ethereum URL",
+	)
+
+	cmd.PersistentFlags().StringVarP(
+		&bootstrapFlags.DeploymentDir,
+		"deployment-dir",
+		"",
+		"./deployments/localhost",
+		"Deployment directory",
+	)
 
 	cmd.PersistentFlags().StringVarP(
 		&bootstrapFlags.ShuttermintURL,
@@ -60,21 +81,20 @@ chain's genesis config.`,
 	)
 	cmd.MarkPersistentFlagRequired("signing-key")
 
-	cmd.PersistentFlags().StringSliceVarP(
-		&bootstrapFlags.Keypers,
-		"keyper",
-		"K",
-		nil,
-		"keyper address")
-	cmd.MarkPersistentFlagRequired("keyper")
 	return cmd
 }
 
-func twothirds(numKeypers int) int {
-	return (2*numKeypers + 2) / 3
-}
-
 func bootstrap() error {
+	ctx := context.Background()
+	ethereumClient, err := ethclient.DialContext(ctx, bootstrapFlags.EthereumURL)
+	if err != nil {
+		return err
+	}
+	contracts, err := deployment.NewContracts(ethereumClient, bootstrapFlags.DeploymentDir)
+	if err != nil {
+		return err
+	}
+
 	shmcl, err := http.New(bootstrapFlags.ShuttermintURL, "/websocket")
 	if err != nil {
 		log.Fatalf("Error connecting to Shuttermint node: %v", err)
@@ -86,19 +106,29 @@ func bootstrap() error {
 	}
 
 	batchConfigIndex := uint64(bootstrapFlags.BatchConfigIndex)
-
-	keypers := []common.Address{}
-	for _, a := range bootstrapFlags.Keypers {
-		if !common.IsHexAddress(a) {
-			return errors.Errorf("--keyper argument '%s' is not an address", a)
-		}
-		keypers = append(keypers, common.HexToAddress(a))
+	cfg, err := contracts.KeypersConfigsList.KeypersConfigs(nil, big.NewInt(int64(batchConfigIndex)))
+	if err != nil {
+		return err
+	}
+	addr, err := contracts.KeypersConfigsList.AddrsSeq(nil)
+	if err != nil {
+		return err
+	}
+	seq, err := contract.NewAddrsSeq(addr, ethereumClient)
+	if err != nil {
+		return err
+	}
+	keypers, err := seq.GetAddrs(nil, cfg.SetIndex)
+	if err != nil {
+		return err
 	}
 
-	threshold := uint64(twothirds(len(keypers)))
+	log.Printf("using config=%+v keypers=%+v", cfg, keypers)
+
+	threshold := cfg.Threshold
 
 	ms := fx.NewRPCMessageSender(shmcl, signingKey)
-	activationBlockNumber := uint64(0)
+	activationBlockNumber := cfg.ActivationBlockNumber
 	batchConfigMsg := shmsg.NewBatchConfig(
 		activationBlockNumber,
 		keypers,
@@ -108,13 +138,13 @@ func bootstrap() error {
 		false,
 	)
 
-	err = ms.SendMessage(context.Background(), batchConfigMsg)
+	err = ms.SendMessage(ctx, batchConfigMsg)
 	if err != nil {
 		return errors.Errorf("Failed to send batch config message: %v", err)
 	}
 
 	batchConfigStartedMsg := shmsg.NewBatchConfigStarted(batchConfigIndex)
-	err = ms.SendMessage(context.Background(), batchConfigStartedMsg)
+	err = ms.SendMessage(ctx, batchConfigStartedMsg)
 	if err != nil {
 		return errors.Errorf("Failed to send start message: %v", err)
 	}
