93cd32760cc14d1f8154a3f03e34c5bea6221ba8
Ralf Schmitt <ralf@brainbot.com>
2022-01-31 09:46:35 +0100

    Configure hardhat to auto generate new blocks every 1.5s
    Otherwise the chainobs may not notice configuration changes, because it uses the
    finality offset.

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -80,6 +80,12 @@ module.exports = {
     collator: 7,
   },
   networks: {
+    hardhat: {
+      mining: {
+        auto: true,
+        interval: 1500,
+      },
+    },
     optimistic: {
       url: "http://127.0.0.1:8545",
       accounts: {
