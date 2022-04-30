9807d04e55338dba2fcd954d6754b3d5136bc8f7
Ralf Schmitt <ralf@brainbot.com>
2022-04-30 14:05:57 +0200

    Determine version via go 1.18's buildinfo
    This makes it possible to use go install with the github URL and end up with an
    executable that knows it's version.
    
    At the moment we still set the version when building via the build-release
    script.

--- rolling-shutter/cmd/shversion/shversion.go
+++ rolling-shutter/cmd/shversion/shversion.go
@@ -5,12 +5,28 @@ package shversion
 import (
 	"fmt"
 	"runtime"
+	"runtime/debug"
 )
 
-var version string = "(unknown)"
+var version string
 
 // Version returns shuttermint's version string.
 func Version() string {
+	if version == "" {
+		info, ok := debug.ReadBuildInfo()
+		if ok {
+			version = info.Main.Version
+			if version == "(devel)" {
+				for _, s := range info.Settings {
+					if s.Key == "vcs.revision" {
+						version = fmt.Sprintf("(devel-%s)", s.Value)
+						break
+					}
+				}
+			}
+		}
+	}
+
 	var raceinfo string
 	if raceDetectorEnabled {
 		raceinfo = ", race detector enabled"
