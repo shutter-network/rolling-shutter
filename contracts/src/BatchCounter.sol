bf558d5e235eee3dd779bd4d94bafc688a3bff4f
jannikluhn <jannik@brainbot.com>
2022-05-03 15:10:50 +0200

    Simplify batch counter contract (#266)

--- contracts/src/BatchCounter.sol
+++ contracts/src/BatchCounter.sol
@@ -2,19 +2,25 @@
 
 pragma solidity =0.8.9;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+contract BatchCounter {
+    error CallerNotZeroAddress();
 
-contract BatchCounter is Ownable {
     event NewBatchIndex(uint64 oldIndex, uint64 newIndex);
     uint64 public batchIndex;
 
-    function increment() external onlyOwner {
+    function increment() external {
+        if (msg.sender != address(0)) {
+            revert CallerNotZeroAddress();
+        }
         emit NewBatchIndex(batchIndex, batchIndex + 1);
         batchIndex += 1;
     }
 
     /// @notice intended for initialization
-    function set(uint64 newBatchIndex) external onlyOwner {
+    function set(uint64 newBatchIndex) external {
+        if (msg.sender != address(0)) {
+            revert CallerNotZeroAddress();
+        }
         emit NewBatchIndex(batchIndex, newBatchIndex);
         batchIndex = newBatchIndex;
     }
