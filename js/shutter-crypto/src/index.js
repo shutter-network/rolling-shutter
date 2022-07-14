eb04ae3c49204b5a2b52dbd790dd5a7057564734
Jannik Luhn <jannik@brainbot.com>
2022-07-14 18:35:57 +0200

    Specify path/url to WASM file at init
    We still include the WASM file in the package.

--- js/shutter-crypto/src/index.js
+++ js/shutter-crypto/src/index.js
@@ -1,57 +1,25 @@
 import "../derived/wasm_exec";
 import { isBrowser, isNode } from "browser-or-node";
 
-let wasm = require("../derived/shutter-crypto.wasm");
-
 const g = global || window || this || self;
 if (typeof g.__wasm_functions__ === "undefined") {
   g.__wasm_functions__ = {};
 }
 
-/* global Go, __non_webpack_require__, __dirname, __webpack_require__ */
-
-/* Slightly modified copy from webpack/runtime/publicPath
- * Webpack's automatic publicPath doesn't work in node, so we need to manually
- * handle it to be compatible with both.
- */
-function getScriptUrl() {
-  var scriptUrl;
-  if (__webpack_require__.g.importScripts)
-    scriptUrl = __webpack_require__.g.location + "";
-  var document = __webpack_require__.g.document;
-  if (!scriptUrl && document) {
-    if (document.currentScript) scriptUrl = document.currentScript.src;
-    if (!scriptUrl) {
-      var scripts = document.getElementsByTagName("script");
-      if (scripts.length) scriptUrl = scripts[scripts.length - 1].src;
-    }
-  }
-  // When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
-  // or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
-  if (!scriptUrl)
-    throw new Error("Automatic publicPath is not supported in this browser");
-  scriptUrl = scriptUrl
-    .replace(/#.*$/, "")
-    .replace(/\?.*$/, "")
-    .replace(/\/[^/]+$/, "/");
-  return scriptUrl;
-}
-
-function init() {
+function init(wasmUrlOrPath) {
   let shutterCrypto;
-  const go = new Go();
+  const go = new Go(); // eslint-disable-line no-undef
   if (isBrowser) {
-    wasm = getScriptUrl() + wasm;
     if ("instantiateStreaming" in WebAssembly) {
       return WebAssembly.instantiateStreaming(
-        fetch(wasm),
+        fetch(wasmUrlOrPath),
         go.importObject
       ).then((obj) => {
         shutterCrypto = obj.instance;
         go.run(shutterCrypto);
       });
     } else {
-      return fetch(wasm)
+      return fetch(wasmUrlOrPath)
         .then((resp) => resp.arrayBuffer())
         .then((bytes) =>
           WebAssembly.instantiate(bytes, go.importObject).then((obj) => {
@@ -61,10 +29,9 @@ function init() {
         );
     }
   } else if (isNode) {
-    const fs = __non_webpack_require__("fs");
-    const path = __non_webpack_require__("path");
+    const fs = __non_webpack_require__("fs"); // eslint-disable-line no-undef
     WebAssembly.instantiate(
-      fs.readFileSync(path.join(__dirname, wasm)),
+      fs.readFileSync(wasmUrlOrPath),
       go.importObject
     ).then((obj) => {
       shutterCrypto = obj.instance;
