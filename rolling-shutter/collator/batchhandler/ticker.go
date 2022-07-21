b031a2f615dad69e2a604550017ff56cad0a59f7
Max Langenfeld <max@brainbot.com>
2022-07-21 14:25:32 +0200

    Add EpochTicker broker
    `EpochTicker` uses the `Broker` subscription class
    in order to distribute a ticker signal to multiple
    subscribers.
    This will get used to feed events to pending batches
    that trigger the creation of the DecryptionTrigger
    as soon as a new Epoch should be initiated, based
    on a constant time interval.
    
    Unlike the `time.Ticker` there is still one problem
    with slow receivers:
    
    Since consumers subscribe to the ticker
    with a non-blocking send,it is possible to miss a tick
    when the receiving loop is busy.
    Unlike in the actual ticker, the consumer now has to wait until the
    next tick is reached.

---
+++ rolling-shutter/collator/batchhandler/ticker.go
@@ -0,0 +1,59 @@
+package batchhandler
+
+import (
+	"time"
+
+	"github.com/rs/zerolog/log"
+
+	"github.com/shutter-network/rolling-shutter/rolling-shutter/medley"
+)
+
+type EpochTicker struct {
+	ticker *time.Ticker
+	broker *medley.Broker[time.Time]
+	stop   chan struct{}
+}
+
+func StartEpochTicker(epochDuration time.Duration) *EpochTicker {
+	return &EpochTicker{
+		ticker: time.NewTicker(epochDuration),
+		broker: medley.NewBroker[time.Time](0, false),
+		stop:   make(chan struct{}),
+	}
+}
+
+func (tick *EpochTicker) Unsubscribe(channel chan time.Time) {
+	tick.broker.Unsubscribe(channel)
+}
+
+func (tick *EpochTicker) Subscribe() chan time.Time {
+	return tick.broker.Subscribe()
+}
+
+func (tick *EpochTicker) Run() {
+	// FIXME there seems to be one problem:
+	// since consumers subscribe to the ticker
+	// with a non-blocking send,
+	// it is possible to skip a tick.
+	// unlike in the actual ticker,
+	// the consumer now has to wait until the
+	// next tick is reached.
+	// In the ticker, a slow consumer will
+	// receive immediately when the time was reached
+	go tick.broker.Start()
+	for {
+		select {
+		case val := <-tick.ticker.C:
+			log.Debug().Str("time", val.String()).Msg("tick broadcast")
+			tick.broker.Publish(val)
+		case <-tick.stop:
+			log.Debug().Msg("ticker received stop signal")
+			tick.broker.Stop()
+			return
+		}
+	}
+}
+
+func (tick *EpochTicker) Stop() {
+	close(tick.stop)
+}
