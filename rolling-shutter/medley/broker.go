602750775c03db73c483d153385e11a5a6367093
Max Langenfeld <max@brainbot.com>
2022-07-22 13:50:27 +0200

    fixup! Add generic observer implementation

--- rolling-shutter/medley/broker.go
+++ rolling-shutter/medley/broker.go
@@ -1,15 +1,13 @@
 package medley
 
 import (
-	"sync"
-
 	"github.com/rs/zerolog/log"
 )
 
 // Broker allows to distribute a value to multiple
 // receiving channels, so that all channels receive the same value.
 // This is different to the Fan-Out pattern where multiple channels
-// are listening for a send on one channel, but only the first channel
+// are listening for a send on one channel but only the first channel
 // to select the receive operation receives this value.
 //
 // The Broker allows to set whether the internal send operation is
@@ -18,125 +16,121 @@ import (
 // for the broker to continue operation. So if one receiver blocks,
 // everything else will block as well.
 // In the non-blocking case, a subscribed channel that is blocking
-// while the published value is sent will miss the value.
+// while the published value is sent will miss the value (depending
+// on the channels buffer).
 //
 // One has to be careful in when to select which case.
 // The blocking behavior can be partially subverted by increasing
 // the subscribers buffer size - but if a subscriber is blocked forever,
 // this will only work until this subscribers buffer is full.
+//
+// The Publish channel field is the means to send a value to the subscribers.
+// If the Publish channel is closed, the broker will stop it's background loop.
 type Broker[T any] struct {
-	mux                  sync.RWMutex
-	stop                 chan struct{}
-	publish              chan T
-	subscribe            chan chan T
-	unsubscribe          chan chan T
-	initialSubscriptions []chan T
-	running              bool
-	nonBlockingSend      bool
+	Publish         chan T
+	stop            chan struct{}
+	subscribe       chan chan T
+	unsubscribe     chan chan T
+	nonBlockingSend bool
 }
 
-func NewBroker[T any](nonBlocking bool) *Broker[T] {
-	return &Broker[T]{
-		stop:                 make(chan struct{}),
-		publish:              make(chan T, 1),
-		subscribe:            make(chan chan T),
-		unsubscribe:          make(chan chan T),
-		nonBlockingSend:      nonBlocking,
-		initialSubscriptions: []chan T{},
-		running:              false,
+func StartNewBroker[T any](nonBlocking bool) *Broker[T] {
+	b := &Broker[T]{
+		Publish:         make(chan T, 1),
+		stop:            make(chan struct{}),
+		subscribe:       make(chan chan T),
+		unsubscribe:     make(chan chan T),
+		nonBlockingSend: nonBlocking,
 	}
-}
 
-// Start starts the Broker's internal loop that processes
-// the subscription and publish operations.
-// initialSubscriptions will be registered
-// before the loop is started.
-// This is useful when the channels must not miss a published value
-// after the start or when the subscribers buffer size
-// should diverge from the default value `bufferSizeSubscriber`
-// set in the constructor.
-func (b *Broker[T]) Start() {
 	subscriptions := map[chan T]struct{}{}
-	for _, sub := range b.initialSubscriptions {
-		log.Debug().Int("num-subscribers", len(subscriptions)).Msg("subscribed initial subscription")
-		subscriptions[sub] = struct{}{}
-	}
-	started := make(chan struct{})
+	// this is started in the background,
+	// without being handled by an errorgroup.
+	// this goroutine could be hanging in the backround
+	// indefinitely
 	go func() {
-		b.mux.Lock()
-		b.running = true
-		b.mux.Unlock()
-		close(started)
+		publish := b.Publish
 		for {
 			select {
 			case <-b.stop:
-				for msgCh := range subscriptions {
-					close(msgCh)
-					delete(subscriptions, msgCh)
+				for subscribeChan := range subscriptions {
+					b.removeSubscriber(subscriptions, subscribeChan)
 				}
-				b.mux.Lock()
-				b.running = false
-				b.mux.Unlock()
 				return
-			case msgCh := <-b.subscribe:
-				subscriptions[msgCh] = struct{}{}
-			case msgCh := <-b.unsubscribe:
-				delete(subscriptions, msgCh)
-				close(msgCh)
-				log.Debug().Int("num-subscribers", len(subscriptions)).Msg("Unsubscribed")
-			case msg := <-b.publish:
-				if b.nonBlockingSend {
-					// use blocking send to make sure all subscribers
-					// got the value,
-					// could block the whole broker (+ successive Publish() calls) if a receiver
-					// is currently busy
-					for msgCh := range subscriptions {
-						msgCh <- msg
-					}
-				} else {
-					// use non-blocking send to protect the broker
-					// and calls to Publish()
-					// receivers could miss published values when they are currently busy
-					// -> higher non-zero 'bufferSizeSubscriber' values could circumvent this somewhat
-					for msgCh := range subscriptions {
-						select {
-						case msgCh <- msg:
-						default:
-						}
-					}
+			case subscribeChan := <-b.subscribe:
+				subscriptions[subscribeChan] = struct{}{}
+			case subscribeChan := <-b.unsubscribe:
+				b.removeSubscriber(subscriptions, subscribeChan)
+			case val, ok := <-publish:
+				if !ok {
+					// the run loop will be closed when the publish channel is closed
+					// that way we still send out all buffered values put in Publish
+					// before we close
+					close(b.stop)
+					publish = nil
+					continue
 				}
+				b.sendToSubscribers(subscriptions, val)
 			}
 		}
 	}()
-	<-started
+	return b
 }
 
-// Stop will close the internal stop channel
-// and will stop the Brokers internal loop
-// as soon as the closed stop channel is selected.
-// A call to Stop() will also cause the subscribed
-// channels to be closed.
-func (b *Broker[_]) Stop() {
-	close(b.stop)
+func (b *Broker[T]) removeSubscriber(subscriptions map[chan T]struct{}, subscribeChan chan T) {
+	delete(subscriptions, subscribeChan)
+	close(subscribeChan)
+	log.Debug().Int("num-subscribers", len(subscriptions)).Msg("Unsubscribed")
 }
 
-// Subscribe registers the channel to the internal
-// send operation. It will receive a published value
-// as soon as the other registered subscribers in line
+func (b *Broker[T]) sendToSubscribers(subscriptions map[chan T]struct{}, value T) {
+	if b.nonBlockingSend {
+		// use non-blocking send to protect the broker
+		// and calls to Publish()
+		// receivers could miss published values when they are currently busy
+		// -> higher non-zero 'bufferSizeSubscriber' values could circumvent this somewhat
+		for msgCh := range subscriptions {
+			select {
+			case msgCh <- value:
+			default:
+			}
+		}
+	} else {
+		// use blocking send to make sure all subscribers
+		// got the value,
+		// could block the whole broker (+ successive Publish() calls) if a receiver
+		// is currently busy
+		for msgCh := range subscriptions {
+			msgCh <- value
+		}
+	}
+}
+
+// Subscribe creates and registers a new channel to
+// receive published values.
+//
+// The returned channel will receive the published value
+// as soon as the previous registered subscribers in line
 // received the value (in the blockingSend case),
 // or as soon as the value is published AND the subscriber
 // is currently waiting on the receive operation
 // (in the non blockingSend case).
-// This is blocking, even if the loop is not running.
-func (b *Broker[T]) Subscribe(channel chan T) {
-	b.mux.RLock()
-	if !b.running {
-		b.initialSubscriptions = append(b.initialSubscriptions, channel)
-		b.mux.RUnlock()
-		return
+//
+// The channel will get closed by the Broker,
+// either when the Broker's loop is stopped,
+// or when the listener asks the Broker to
+// unsubscribe the channel via the Unsubscribe() method.
+func (b *Broker[T]) Subscribe(chanSize int) chan T {
+	channel := make(chan T, chanSize)
+	select {
+	case <-b.stop:
+		// Don't block the Subscribe() method when the broker
+		// is stopped.
+		// To comply with the interface, return a closed channel.
+		close(channel)
+	case b.subscribe <- channel:
 	}
-	b.mux.RUnlock()
-	b.subscribe <- channel
+	return channel
 }
 
 // Unsubscribe deregisters the subscribed channel
@@ -144,46 +138,26 @@ func (b *Broker[T]) Subscribe(channel chan T) {
 // subscribed channel.
 // This is a noop when the loop is not running.
 func (b *Broker[T]) Unsubscribe(channel chan T) {
-	b.mux.RLock()
-	if !b.running {
-		// return directly in case the loop is not running,
-		// otherwise this would block forever
-		b.mux.RUnlock()
-		return
-	}
-	b.mux.RUnlock()
-	for {
-		select {
-		case <-b.stop:
-			return
-		case b.unsubscribe <- channel:
-			return
-		case <-channel:
-			// Allow to flush the message channel
-		}
-	}
-}
-
-// Publish distributes the `value` to
-// all subscribers as soon as the
-// internal loop is not busy anymore.
-// Note that this could potentially block
-// if the loop is busy, although the internal
-// publish channel has a buffer of 1.
-func (b *Broker[T]) Publish(value T) {
-	// noop when not running
-	b.mux.RLock()
-	if !b.running {
-		// return directly in case the loop is not running,
-		// otherwise this would block forever
-		b.mux.RUnlock()
-		return
-	}
-	b.mux.RUnlock()
 	select {
 	case <-b.stop:
+		// Don't block the Unsubscribe() method when the broker
+		// is stopped
+
+		// Contrary to the Subscribe, this is a noop and the channel
+		// is not closed here.
+		// This is because there could be a race (close of closed channel):
+		// The observer doesn't know when the broker is stopped
+		// so he could call unsubscribe just after the broker
+		// being stopped and the channel being closed.
+
+		// If the passed in channel IS a subscription channel instantiated
+		// by the broker, it would be closed during the shutdown
+		// of the broker anyways.
+
+		// Calling unsubscribe with a channel not instantiated by the
+		// is discouraged!
 		return
-	case b.publish <- value:
+	case b.unsubscribe <- channel:
 		return
 	}
 }
