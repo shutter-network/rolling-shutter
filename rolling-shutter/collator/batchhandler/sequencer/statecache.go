ca5268b744d8494a6e18aa966340fc40e071c4a7
Max Langenfeld <max@brainbot.com>
2022-07-27 17:21:18 +0200

    Make sequencer.Cached threadsafe per address
    Before the Cached class was not threadsafe,
    so that even intertwined calls to the getter
    methods could result in a write-race due
    to the write of polled initial values from the
    sequencer.
    
    In this commit, a new class `KeyedMutex` is introduced,
    which allows Lock/Unlock RLock/RUnlock operations per unique key.
    Interally, `KeyedMutex` keeps track of pending Lock/RLock calls
    per key, and when the last Unlock/RUnlock call has finished,
    the mutex is purged from the internal state to avoid keeping
    inactive mutexes in memory.
    
    The `Cached` class now holds 2 instances of MapLocks in order
    to lock calls to the nonces and balances caches on a per address
    basis.

--- rolling-shutter/collator/batchhandler/sequencer/statecache.go
+++ rolling-shutter/collator/batchhandler/sequencer/statecache.go
@@ -3,6 +3,9 @@ package sequencer
 import (
 	"context"
 	"math/big"
+	"reflect"
+	"sync"
+	"sync/atomic"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/pkg/errors"
@@ -27,26 +30,89 @@ type EthClient interface {
 	NonceAt(context.Context, common.Address, *big.Int) (uint64, error)
 }
 
-func NewChainBatchCache(client EthClient, atBlockNumber *big.Int) *ChainBatchCache {
-	return &ChainBatchCache{
+type countedMux struct {
+	mux *sync.RWMutex
+	cnt uint32
+}
+
+func NewKeyedMutex[T comparable]() *KeyedMutex[T] {
+	return &KeyedMutex[T]{mu: sync.RWMutex{}, muxs: make(map[T]*countedMux)}
+}
+
+type KeyedMutex[T comparable] struct {
+	mu   sync.RWMutex
+	muxs map[T]*countedMux
+}
+
+func (ml *KeyedMutex[T]) call(method string, v T, delta int) {
+	ml.mu.Lock()
+	vmu, ok := ml.muxs[v]
+	if !ok {
+		vmu = &countedMux{
+			mux: &sync.RWMutex{},
+			cnt: 0,
+		}
+		ml.muxs[v] = vmu
+	}
+	var addUint32 uint32
+	if delta < 0 {
+		// recommended operation to substract from counter
+		addUint32 = ^uint32((-delta) - 1)
+	} else {
+		addUint32 = uint32(delta)
+	}
+	atomic.AddUint32(&vmu.cnt, addUint32)
+	ml.mu.Unlock()
+	// e.g. for method="Lock", this translates to:
+	// vmu.mux.Lock()
+	reflect.ValueOf(vmu.mux).MethodByName(method).Call([]reflect.Value{})
+	ml.mu.Lock()
+	if vmu.cnt <= 0 {
+		delete(ml.muxs, v)
+	}
+	ml.mu.Unlock()
+}
+
+func (ml *KeyedMutex[T]) Lock(v T) {
+	ml.call("Lock", v, 1)
+}
+
+func (ml *KeyedMutex[T]) Unlock(v T) {
+	ml.call("Unlock", v, -1)
+}
+
+func (ml *KeyedMutex[T]) RLock(v T) {
+	ml.call("RLock", v, 1)
+}
+
+func (ml *KeyedMutex[T]) RUnlock(v T) {
+	ml.call("RUnlock", v, -1)
+}
+
+func NewCached(client EthClient, atBlockNumber *big.Int) *Cached {
+	return &Cached{
+		balances:      make(map[common.Address]*big.Int),
+		nonces:        make(map[common.Address]uint64),
+		balancesLocks: NewKeyedMutex[common.Address](),
+		noncesLocks:   NewKeyedMutex[common.Address](),
 		Client:        client,
-		AtBlockNumber: nil,
-		balances:      make(map[common.Address]*big.Int, 0),
-		nonces:        make(map[common.Address]uint64, 0),
+		AtBlockNumber: atBlockNumber,
 	}
 }
 
-// ChainBatchCache tracks the state of account's nonces and balances
+// Cached tracks the state of account's nonces and balances
 // for a certain Batch.
 // If an address is not cached yet, it polls initial balances or nonces
 // on a GetBalance or GetNonce call from the underlying ethereum node via the
-// ChainBatchCache.Client. Then the value is cached and never polled again for
+// Cached.Client. Then the value is cached and never polled again for
 // that address.
 // This allows to poll chain-state and then modify it locally, e.g. while
 // accepting user transactions to be proposed as the next block to the sequencer.
-type ChainBatchCache struct {
-	balances map[common.Address]*big.Int
-	nonces   map[common.Address]uint64
+type Cached struct {
+	balances      map[common.Address]*big.Int
+	nonces        map[common.Address]uint64
+	balancesLocks *KeyedMutex[common.Address]
+	noncesLocks   *KeyedMutex[common.Address]
 
 	Client        EthClient
 	AtBlockNumber *big.Int
@@ -54,7 +120,14 @@ type ChainBatchCache struct {
 
 // GetBalance polls and caches the state of account `a` balance at the
 // block number ChainBatchCache.AtBlockNumber.
-func (c *ChainBatchCache) GetBalance(ctx context.Context, a common.Address) (*big.Int, error) {
+func (c *Cached) GetBalance(ctx context.Context, a common.Address) (*big.Int, error) {
+	// write lock because we eventually write to the dict
+	c.balancesLocks.Lock(a)
+	defer c.balancesLocks.Unlock(a)
+	return c.getBalance(ctx, a)
+}
+
+func (c *Cached) getBalance(ctx context.Context, a common.Address) (*big.Int, error) {
 	var err error
 
 	bal, exists := c.balances[a]
@@ -72,8 +145,14 @@ func (c *ChainBatchCache) GetBalance(ctx context.Context, a common.Address) (*bi
 // If no balance is cached yet, SubBalance will conduct a call to the ethereum
 // node to get the state of the balance before modifying it.
 // The modified value is then persisted in the internal state cache.
-func (c *ChainBatchCache) SubBalance(ctx context.Context, a common.Address, diff *big.Int) error {
-	old, err := c.GetBalance(ctx, a)
+func (c *Cached) SubBalance(ctx context.Context, a common.Address, diff *big.Int) error {
+	c.balancesLocks.Lock(a)
+	defer c.balancesLocks.Unlock(a)
+	return c.subBalance(ctx, a, diff)
+}
+
+func (c *Cached) subBalance(ctx context.Context, a common.Address, diff *big.Int) error {
+	old, err := c.getBalance(ctx, a)
 	if err != nil {
 		return err
 	}
@@ -89,8 +168,14 @@ func (c *ChainBatchCache) SubBalance(ctx context.Context, a common.Address, diff
 // If no balance is cached yet, AddBalance will conduct a call to the ethereum
 // node to get the state of the balance before modifying it.
 // The modified value is then persisted in the internal state cache.
-func (c *ChainBatchCache) AddBalance(ctx context.Context, a common.Address, diff *big.Int) error {
-	old, err := c.GetBalance(ctx, a)
+func (c *Cached) AddBalance(ctx context.Context, a common.Address, diff *big.Int) error {
+	c.balancesLocks.Lock(a)
+	defer c.balancesLocks.Unlock(a)
+	return c.addBalance(ctx, a, diff)
+}
+
+func (c *Cached) addBalance(ctx context.Context, a common.Address, diff *big.Int) error {
+	old, err := c.getBalance(ctx, a)
 	if err != nil {
 		return err
 	}
@@ -100,8 +185,13 @@ func (c *ChainBatchCache) AddBalance(ctx context.Context, a common.Address, diff
 
 // GetNonce polls and caches the state of account `a` balance at the
 // block number ChainBatchCache.AtBlockNumber.
-func (c *ChainBatchCache) GetNonce(ctx context.Context, a common.Address) (uint64, error) {
-	var err error
+func (c *Cached) GetNonce(ctx context.Context, a common.Address) (uint64, error) {
+	c.noncesLocks.Lock(a)
+	defer c.noncesLocks.Unlock(a)
+	var (
+		err   error
+		nonce uint64
+	)
 	nonce, exists := c.nonces[a]
 	if !exists {
 		nonce, err = c.Client.NonceAt(ctx, a, c.AtBlockNumber)
@@ -116,13 +206,19 @@ func (c *ChainBatchCache) GetNonce(ctx context.Context, a common.Address) (uint6
 // SetNonce sets the value in the nonce cache of account `a` to value `nonce`.
 // Once this is set, a call to GetNonce() will not poll the node but simply
 // return the set value.
-func (c *ChainBatchCache) SetNonce(a common.Address, nonce uint64) {
+func (c *Cached) SetNonce(a common.Address, nonce uint64) {
+	c.noncesLocks.Lock(a)
+	defer c.noncesLocks.Unlock(a)
 	c.nonces[a] = nonce
 }
 
-func (c *ChainBatchCache) Purge(a common.Address) {
+func (c *Cached) Purge(a common.Address) {
+	c.noncesLocks.Lock(a)
+	defer c.noncesLocks.Unlock(a)
+	c.balancesLocks.Lock(a)
+	defer c.balancesLocks.Unlock(a)
 	delete(c.nonces, a)
 	delete(c.balances, a)
 }
 
-var _ State = (*ChainBatchCache)(nil)
+var _ State = (*Cached)(nil)
