2868beb6118989edb9dd16fa2e73a8c35c26681f
Max Langenfeld <max@brainbot.com>
2022-10-28 17:10:46 +0200

    feat(mocksequencer): add `shutter_getTransactionByHash` RPC method

--- rolling-shutter/mocksequencer/rpc/service_shutter.go
+++ rolling-shutter/mocksequencer/rpc/service_shutter.go
@@ -2,8 +2,11 @@ package rpc
 
 import (
 	"context"
+	"math/big"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	ethrpc "github.com/ethereum/go-ethereum/rpc"
 	"github.com/pkg/errors"
 	txtypes "github.com/shutter-network/txtypes/types"
 
@@ -27,6 +30,41 @@ func (s *ShutterService) Name() string {
 
 func (s *ShutterService) BatchIndex() hexutil.Uint64 {
 	return hexutil.Uint64(s.processor.BatchIndex)
+}
+
+func (s *ShutterService) GetTransactionByHash(hash common.Hash) (*txtypes.TransactionData, error) {
+	s.processor.Mux.RLock()
+	defer s.processor.Mux.RUnlock()
+
+	txID, ok := s.processor.Txs[hash]
+	if !ok {
+		// ETH JSON RPC returns "null" when not found
+		return nil, nil
+	}
+	blockHash := ethrpc.BlockNumberOrHash{BlockHash: &txID.BlockHash}
+	b, err := s.processor.GetBlock(blockHash)
+	if err != nil {
+		// this shouldn't happen
+		return nil, rpcerrors.ExtractRPCError(err)
+	}
+	tx := b.Transactions[txID.Index]
+
+	rpcTx := tx.TransactionData()
+
+	if b.Hash != (common.Hash{}) {
+		rpcTx.BlockHash = &b.Hash
+		rpcTx.BlockNumber = (*hexutil.Big)(new(big.Int).SetUint64(b.Number))
+		index := uint64(txID.Index)
+		rpcTx.TransactionIndex = (*hexutil.Uint64)(&index)
+
+		//nolint:godox //this is not worth an issue at the moment
+		// TODO(ezdac) passing this as well would be nice,
+		// but it is not required due to the already passed decrypted tx.Payload
+		//
+		// This would require us to save the decryption key
+		// result.DecryptionKey = (*hexutil.Bytes)(&decryptionKey)
+	}
+	return rpcTx, nil
 }
 
 func (s *ShutterService) SubmitBatch(ctx context.Context, batchTransaction string) (string, error) {
