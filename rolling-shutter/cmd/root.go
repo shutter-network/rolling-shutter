5b9f02d04362d38891430a680bf5c7c06eda8300
Max Langenfeld <max@brainbot.com>
2023-01-18 14:20:47 +0100

    feat: add `--loglevel` and `--logformat` options to root command
    This will port the former `--log` root commandline option from the
    stdlibs `log` flags to the `zerolog` configuration.
    The option is renamed to `--logformat`
    
    Additionally, a new root commandline option `--loglevel`, that
    allows to chose between "debug", "warn" and "info" log-levels.

--- rolling-shutter/cmd/root.go
+++ rolling-shutter/cmd/root.go
@@ -6,9 +6,11 @@ import (
 	"os"
 	"strings"
 
+	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+	flag "github.com/spf13/pflag"
 
 	"github.com/shutter-network/rolling-shutter/rolling-shutter/cmd/bootstrap"
 	"github.com/shutter-network/rolling-shutter/rolling-shutter/cmd/chain"
@@ -24,13 +26,105 @@ import (
 	"github.com/shutter-network/rolling-shutter/rolling-shutter/medley"
 )
 
-var logformat string
+var (
+	logFormatArg  string
+	logFormatName string = "logformat"
+	logLevelArg   string
+	logLevelName  string = "loglevel"
+)
 
-func initZerologCallerMarshal() {
-	pathsep := string(os.PathSeparator)
-	zerolog.CallerMarshalFunc = func(file string, line int) string {
-		return fmt.Sprintf("%s:%d", file[1+strings.LastIndex(file, pathsep):], line)
+func errorForFlag(flg *flag.Flag) error {
+	if flg != nil {
+		return errors.Errorf("failed to parse '%s' option. usage: '%s'", flg.Name, flg.Usage)
 	}
+	return errors.Errorf("'%s' option not specified", logFormatName)
+}
+
+func configureCaller(l zerolog.Logger, short bool) zerolog.Logger {
+	if short {
+		pathsep := string(os.PathSeparator)
+		// default is long filename
+		zerolog.CallerMarshalFunc = func(file string, line int) string {
+			return fmt.Sprintf("%s:%d", file[1+strings.LastIndex(file, pathsep):], line)
+		}
+	}
+	return l.With().Caller().Logger()
+}
+
+func configureTime(l zerolog.Logger) zerolog.Logger {
+	zerolog.TimeFieldFormat = "2006/01/02 15:04:05.000000"
+	return l.With().Timestamp().Logger()
+}
+
+func setupLogging(cmd *cobra.Command) (zerolog.Logger, error) {
+	// create a basic logger with stdout writer
+	// we will change the writer later
+
+	l := zerolog.New(os.Stdout)
+	exclude := []string{}
+	// change the "message" field name, so that
+	// it doesn't collide with e.g. logging of
+	// shutter "message"
+	zerolog.MessageFieldName = "log"
+
+	logFormatFlag := cmd.PersistentFlags().Lookup(logFormatName)
+	if logFormatFlag == nil {
+		// this should not happen due to user error,
+		// since the flag should have a default value attached
+		return l, errors.Errorf("flag '%s' not found", logFormatName)
+	}
+	switch logFormatFlag.Value.String() {
+	case "max", "long":
+		l = configureTime(l)
+		l = configureCaller(l, true)
+	case "short":
+		// no time/date logging
+		l = configureCaller(l, true)
+		exclude = []string{
+			zerolog.TimestampFieldName,
+		}
+	case "min":
+		// no time/date logging
+		// no caller logging
+		exclude = []string{
+			zerolog.TimestampFieldName,
+			zerolog.CallerFieldName,
+		}
+	default:
+		return l, errorForFlag(logFormatFlag)
+	}
+
+	logLevelFlag := cmd.PersistentFlags().Lookup(logLevelName)
+	if logFormatFlag == nil {
+		// this should not happen due to user error,
+		// since the flag should have a default value attached
+		return l, errors.Errorf("flag '%s' not found", logLevelName)
+	}
+	switch logLevelFlag.Value.String() {
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	default:
+		return l, errorForFlag(logLevelFlag)
+	}
+
+	// reset the writer
+	l = l.Output(zerolog.ConsoleWriter{
+		NoColor:    true,
+		Out:        os.Stderr,
+		TimeFormat: zerolog.TimeFieldFormat,
+		PartsOrder: []string{
+			zerolog.TimestampFieldName,
+			zerolog.LevelFieldName,
+			zerolog.CallerFieldName,
+			zerolog.MessageFieldName,
+		},
+		PartsExclude: exclude,
+	})
+	return l, nil
 }
 
 func Cmd() *cobra.Command {
@@ -43,47 +137,26 @@ func Cmd() *cobra.Command {
 			if err != nil {
 				return err
 			}
-			initZerologCallerMarshal()
-			zerolog.TimeFieldFormat = "2006/01/02 15:04:05.000000"
-			log.Logger = log.Output(zerolog.ConsoleWriter{
-				NoColor:    true,
-				Out:        os.Stderr,
-				TimeFormat: zerolog.TimeFieldFormat,
-				PartsOrder: []string{
-					zerolog.TimestampFieldName,
-					zerolog.LevelFieldName,
-					zerolog.CallerFieldName,
-					zerolog.MessageFieldName,
-				},
-			}).With().Timestamp().Caller().Logger()
-			zerolog.SetGlobalLevel(zerolog.InfoLevel)
-			var flags int
 
-			switch logformat {
-			case "min":
-			case "short":
-				flags = log.Lshortfile
-			case "long":
-				flags = log.LstdFlags | log.Lshortfile | log.Lmicroseconds
-			case "max":
-				flags = log.LstdFlags | log.Llongfile | log.Lmicroseconds
-			default:
-				return fmt.Errorf(
-					"bad log value, possible values: min, short, long, max",
-				)
+			logger, err := setupLogging(cmd.Root())
+			if err != nil {
+				return errors.Wrap(err, "failed to setup logging")
 			}
-
-			log.SetFlags(flags)
+			log.Logger = logger
 			return nil
 		},
-		Run:          medley.ShowHelpAndExit,
-		SilenceUsage: true,
 	}
 	cmd.PersistentFlags().StringVar(
-		&logformat,
-		"log",
+		&logFormatArg,
+		logFormatName,
 		"long",
 		"set log format, possible values:  min, short, long, max",
+	)
+	cmd.PersistentFlags().StringVar(
+		&logLevelArg,
+		logLevelName,
+		"info",
+		"set log level, possible values:  warn, info, debug",
 	)
 	cmd.AddCommand(bootstrap.Cmd())
 	cmd.AddCommand(chain.Cmd())
