196478da5069aab81169e4faaf4adaa2ca0fee60
Ralf Schmitt <ralf@brainbot.com>
2023-03-27 12:08:48 +0200

    Add Runner.Defer

--- rolling-shutter/medley/service/service.go
+++ rolling-shutter/medley/service/service.go
@@ -3,13 +3,18 @@ package service
 
 import (
 	"context"
+	"sync"
 
 	"golang.org/x/sync/errgroup"
 )
 
 type Runner interface {
+	// Go starts a background go routine in an error group.
 	Go(f func() error)
+	// StartService starts the given (sub)services.
 	StartService(s ...Service) error
+	// Defer registers the given function to be called when shutting down the service.
+	Defer(f func())
 }
 
 type Service interface {
@@ -17,12 +22,20 @@ type Service interface {
 }
 
 type runner struct {
-	group *errgroup.Group
-	ctx   context.Context
+	group         *errgroup.Group
+	ctx           context.Context
+	mux           sync.Mutex
+	shutdownFuncs []func()
 }
 
 func (r *runner) Go(f func() error) {
 	r.group.Go(f)
+}
+
+func (r *runner) Defer(f func()) {
+	r.mux.Lock()
+	defer r.mux.Unlock()
+	r.shutdownFuncs = append(r.shutdownFuncs, f)
 }
 
 func (r *runner) StartService(services ...Service) error {
@@ -35,12 +48,22 @@ func (r *runner) StartService(services ...Service) error {
 	return nil
 }
 
+func (r *runner) runShutdownFuncs() {
+	r.mux.Lock()
+	defer r.mux.Unlock()
+
+	for i := len(r.shutdownFuncs) - 1; i >= 0; i-- {
+		r.shutdownFuncs[i]()
+	}
+}
+
 func Run(ctx context.Context, services ...Service) error {
 	group, ctx := errgroup.WithContext(ctx)
 	r := runner{group: group, ctx: ctx}
 	group.Go(func() error {
 		return r.StartService(services...)
 	})
+	defer r.runShutdownFuncs()
 	return group.Wait()
 }
 
