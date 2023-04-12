8086777bd7edbc3063cfe2efb557c03ae7a6b3e7
Ulrich Petri <ulo@ulo.pe>
2023-04-12 19:16:50 +0200

    Add a snapshot dummy jsonrpc server
    A simple JSONRPC server implemented in Python that mimicks the snapshot
    interface for local testing.

---
+++ tools/snapshot/dummyserver/dummyserver/main.py
@@ -0,0 +1,28 @@
+import click
+from jsonrpcserver import method, Result, Success, serve
+from structlog import get_logger
+
+log = get_logger(__name__)
+
+
+@method
+def shutter_set_proposal_key(proposalId: str, key: str) -> Result:
+    log.info("JSONRPC", method='shutter_set_proposal_key', proposal_id=proposalId, key=key)
+    return Success(True)
+
+
+@method
+def shutter_set_eon_pubkey(eonId: str, key: str) -> Result:
+    log.info("JSONRPC", method='shutter_set_eon_pubkey', eon_id=eonId, key=key)
+    return Success(True)
+
+
+@click.command()
+@click.option('-h', '--host', default='0.0.0.0')
+@click.option('-p', '--port', default=5000)
+def main(host: str, port: int) -> None:
+    serve(host, port)
+
+
+if __name__ == "__main__":
+    main()
