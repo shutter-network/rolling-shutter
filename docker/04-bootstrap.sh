d16b99df7193a735abe7b481466e127bfcd1506a
Ulrich Petri <ulo@ulo.pe>
2023-09-25 11:31:27 +0200

    Remove sentry nodes from docker setup
    Currently all validators connect to validator-0 instead of bootstrap (see `02-init-chain.sh:56`)

--- docker/04-bootstrap.sh
+++ docker/04-bootstrap.sh
@@ -7,5 +7,5 @@ echo "Submitting bootstrap transaction"
 $DC run --rm --no-deps --entrypoint /rolling-shutter chain-0-validator bootstrap \
     --deployment-dir /deployments/dockerGeth \
     --ethereum-url http://geth:8545 \
-    --shuttermint-url http://chain-0-sentry:${TM_RPC_PORT} \
+    --shuttermint-url http://chain-0-validator:${TM_RPC_PORT} \
     --signing-key 479968ffa5ee4c84514a477a8f15f3db0413964fd4c20b08a55fed9fed790fad
--- docker/05-test.sh
+++ docker/05-test.sh
@@ -3,9 +3,14 @@
 source ./common.sh
 set +ex
 
-echo "Testing decryption key generation"
 EPOCH_ID=$(LC_ALL=C tr -dc 'a-f0-9' </dev/urandom | head -c64)
-json_body="{\"jsonrpc\": \"2.0\", \"method\": \"get_decryption_key\", \"id\": 1, \"params\": [\"1\", \"${EPOCH_ID}\"]}"
+EON_ID=$($DC exec db psql -U postgres -d keyper-0 -t -c 'select max(eon) from eons;' | tr -d '[:space:]')
+if [[ -z $EON_ID ]]; then
+    echo "No eonId found in keyper-0 db. Did you run bootstrap?"
+    exit 1
+fi
+json_body="{\"jsonrpc\": \"2.0\", \"method\": \"get_decryption_key\", \"id\": 1, \"params\": [\"${EON_ID}\", \"${EPOCH_ID}\"]}"
+echo "Testing decryption key generation for eonId ${EON_ID} and epoch ${EPOCH_ID}"
 curl -XGET http://localhost:8754/api/v1/rpc -d "${json_body}"
 sleep 5
 DECRYPTION_KEY_MSGS=$(${DC} logs snapshot | grep ${EPOCH_ID} | grep -c decryptionKey)
