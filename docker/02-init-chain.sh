51f9d51c2537d7d8b8533667e4f79e8dfda8b24d
Konrad Feldmeier <konrad@brainbot.com>
2023-09-25 15:30:14 +0200

    Simplify init-chain script

--- docker/02-init-chain.sh
+++ docker/02-init-chain.sh
@@ -16,13 +16,19 @@ ${BB} rm -rf data/deployments
 # has geth as dependency
 $DC up deploy-contracts
 
+# setup chain-seed
 $DC run --rm --no-deps chain-seed init \
     --root /chain \
     --blocktime 1 \
     --listen-address tcp://0.0.0.0:${TM_RPC_PORT} \
     --role seed
 
-for num in 0 1 2 3; do
+seed_node=$(cat data/chain-seed/config/node_key.json.id)@chain-seed:${TM_P2P_PORT}
+
+${BB} sed -i "/^moniker/c\moniker = \"chain-seed\"" data/chain-seed/config/config.toml
+
+# configure validators and keypers 0-3
+for num in {0..3}; do
     validator_cmd=chain-$num-validator
 
     $DC run --rm --no-deps ${validator_cmd} init \
@@ -32,31 +38,26 @@ for num in 0 1 2 3; do
         --listen-address tcp://0.0.0.0:${TM_RPC_PORT} \
         --role validator
 
-    ${BB} sed -i "/ValidatorPublicKey/c\ValidatorPublicKey = \"$(cat data/${validator_cmd}/config/priv_validator_pubkey.hex)\"" /config/keyper-${num}.toml
+    validator_id=$(cat data/${validator_cmd}/config/node_key.json.id)
+    validator_node=${validator_id}@${validator_cmd}:${TM_P2P_PORT}
+    validator_config_path=data/${validator_cmd}/config/config.toml
 
+    # share genesis
     if [ $num -eq 0 ]; then
-        for destination in data/chain-seed/config/ data/chain-{1..3}-validator/config/ ; do
+        for destination in data/chain-seed/config/ data/chain-{1..3}-validator/config/; do
             ${BB} cp -v data/chain-0-validator/config/genesis.json "${destination}"
         done
     fi
-done
 
-seed_node=$(cat data/chain-seed/config/node_key.json.id)@chain-seed:${TM_P2P_PORT}
-validator_0_node=$(cat data/chain-0-validator/config/node_key.json.id)@chain-0-validator:${TM_P2P_PORT}
+    # set validator publickey for keyper
+    ${BB} sed -i "/ValidatorPublicKey/c\ValidatorPublicKey = \"$(cat data/${validator_cmd}/config/priv_validator_pubkey.hex)\"" /config/keyper-${num}.toml
 
-${BB} sed -i "/^moniker/c\moniker = \"chain-seed\"" data/chain-seed/config/config.toml
-
-for num in {0..3}; do
-    validator_cmd=chain-$num-validator
-
-    validator_id=$(cat data/${validator_cmd}/config/node_key.json.id)
-    validator_node=${validator_id}@${validator_cmd}:${TM_P2P_PORT}
-
-    # set seed node for validator
-    ${BB} sed -i "/^bootstrap_peers =/c\bootstrap_peers = \"${validator_0_node}\"" data/${validator_cmd}/config/config.toml
-    ${BB} sed -i "/^external_address =/c\external_address = \"${validator_cmd}:${TM_P2P_PORT}\"" data/${validator_cmd}/config/config.toml
-    ${BB} sed -i "/^pex/c\pex = true" data/${validator_cmd}/config/config.toml
-    ${BB} sed -i "/^moniker/c\moniker = \"${validator_cmd}\"" data/${validator_cmd}/config/config.toml
+    # set seed node for chain bootstrap
+    ${BB} sed -i "/^bootstrap_peers =/c\bootstrap_peers = \"${seed_node}\"" "${validator_config_path}"
+    # fix external address for docker internal communication
+    ${BB} sed -i "/^external_address =/c\external_address = \"${validator_cmd}:${TM_P2P_PORT}\"" "${validator_config_path}"
+    # give a nice name
+    ${BB} sed -i "/^moniker/c\moniker = \"${validator_cmd}\"" "${validator_config_path}"
 
 done
 
