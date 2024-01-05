152a30d6161af09285e92f224faa48f77964b208
Maximilian Langenfeld <max@brainbot.com>
2024-01-05 15:27:32 +0100

    feat(op): allow to retrieve keyperset by index for op-bootstrap

--- rolling-shutter/keyperimpl/optimism/bootstrap/keyperset.go
+++ rolling-shutter/keyperimpl/optimism/bootstrap/keyperset.go
@@ -3,9 +3,11 @@ package bootstrap
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"os"
 
 	"github.com/shutter-network/rolling-shutter/rolling-shutter/keyperimpl/optimism/sync"
+	"github.com/shutter-network/rolling-shutter/rolling-shutter/keyperimpl/optimism/sync/event"
 	"github.com/shutter-network/rolling-shutter/rolling-shutter/medley/encodeable/number"
 )
 
@@ -17,7 +19,19 @@ func GetKeyperSet(ctx context.Context, config *Config) error {
 	if err != nil {
 		return err
 	}
-	keyperSet, err := sl2.GetKeyperSetForBlock(ctx, number.LatestBlock)
+	var keyperSet *event.KeyperSet
+	switch {
+	case config.ByIndex == nil && config.ByActivationBlockNumber == nil:
+		keyperSet, err = sl2.GetKeyperSetForBlock(ctx, number.LatestBlock)
+	case config.ByIndex == nil && config.ByActivationBlockNumber != nil:
+		keyperSet, err = sl2.GetKeyperSetForBlock(ctx, config.ByActivationBlockNumber)
+	case config.ByIndex != nil && config.ByActivationBlockNumber == nil:
+		keyperSet, err = sl2.GetKeyperSetByIndex(ctx, *config.ByIndex)
+	case config.ByIndex != nil && config.ByActivationBlockNumber != nil:
+		return errors.New("can only retrieve keyper set by either index or activation-blocknumber")
+	default:
+		return nil
+	}
 	if err != nil {
 		return err
 	}
