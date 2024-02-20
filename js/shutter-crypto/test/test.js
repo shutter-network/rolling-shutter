b825a7bec9a3590a2e3125749ce182b3ff13eda7
Konrad Feldmeier <konrad@brainbot.com>
2024-02-20 13:25:02 +0100

    Implement json tests for WASM
    This adds a test step for running the tests generated by
    ```
    ./bin/rolling-shutter crypto testdata
    ```
    against the WASM library.
    
    Configuration of the test file location is done through the environment
    variable
    ```
    SHUTTER_WASM_JSON_TESTDATA
    ```

--- js/shutter-crypto/test/test.js
+++ js/shutter-crypto/test/test.js
@@ -1,7 +1,10 @@
 const shutter = require("../dist/shutter-crypto");
 const ethers = require("ethers");
+const fs = require("node:fs");
+const util = require("node:util");
+const process = require("node:process");
+const Buffer = require("buffer").Buffer;
 
-var encrypted;
 describe("Test shutter crypto", () => {
   test("init the wasm library", async () => {
     try {
@@ -10,8 +13,10 @@ describe("Test shutter crypto", () => {
       expect(error).toBeNone();
     }
   });
+  var encrypted;
+  var msg;
   test("encrypt a message", async () => {
-    const msg = ethers.getBytes(Buffer.from("a message"));
+    msg = ethers.getBytes(Buffer.from("a message"));
     const hexString =
       "0b4e86e0ed51ef774210d1c0fe0be6f1b4f0695d5d396b3b003547f752ac82e316375aa37b1739c9c8472b1b5ae09477565bf9d2c0d7db0c39576f4615d32703262d5854bfbac8a60eb6d227f397289e6e51f979b56476b7f7f32a45ede7a61f21d893a54ab6e65b283342adc41d53df5432569c6a8c2304921bce3ea148efb4";
     const eonkey = Uint8Array.from(Buffer.from(hexString, "hex"));
@@ -41,6 +46,7 @@ describe("Test shutter crypto", () => {
     } catch (error) {
       expect(error).toBeNull();
     }
+    expect(ethers.hexlify(decrypted)).toEqual(ethers.hexlify(msg));
   });
 });
 
@@ -93,5 +99,122 @@ describe("Test known values (values obtained from 'rolling-shutter crypto encryp
       "a message".split("").map((c) => c.charCodeAt())
     );
     expect(decrypted).toEqual(expected);
+  });
+});
+
+function get_testdata() {
+  const env_name = "SHUTTER_WASM_JSON_TESTDATA";
+  try {
+    var testdata = process.env[env_name];
+    if (!testdata) {
+      testdata = "./shutter_testdata.json";
+    }
+    var json_tests = fs.readFileSync(testdata, "utf-8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        return data;
+      }
+    });
+  } catch (err) {
+    console.error(
+      `Could not read the json test file at '${testdata}'.
+Run 'rolling-shutter crypto testdata /path/to/crypto_test.json'
+and set/export ${env_name}=/path/to/crypto_test.json`
+    );
+  }
+  const test_cases = JSON.parse(json_tests);
+
+  const _encryption_tests = test_cases.filter((tc) => {
+    return tc["type"] === "encryption";
+  });
+  const encryption_tests = _encryption_tests.map((tc) => {
+    return [
+      util.format("[%s] %s: %s", tc.id, tc.name, tc.description),
+      tc.test_data,
+    ];
+  });
+  const _decryption_tests = test_cases.filter((tc) => {
+    return tc["type"] === "decryption";
+  });
+  const decryption_tests = _decryption_tests.map((tc) => {
+    return [
+      util.format("[%s] %s: %s", tc.id, tc.name, tc.description),
+      tc.test_data,
+    ];
+  });
+  const _verification_tests = test_cases.filter((tc) => {
+    return tc["type"] === "verification";
+  });
+  const verification_tests = _verification_tests.map((tc) => {
+    return [
+      util.format("[%s] %s: %s", tc.id, tc.name, tc.description),
+      tc.test_data,
+    ];
+  });
+  return [encryption_tests, decryption_tests, verification_tests];
+}
+
+describe("Run the json tests", () => {
+  var [encryption_tests, decryption_tests, verification_tests] = get_testdata();
+  test.each(encryption_tests)("%s", async (name, test_data) => {
+    const msg = ethers.getBytes(Buffer.from(test_data.message.slice(2), "hex"));
+    const eonkey = ethers.getBytes(
+      Buffer.from(test_data.eon_public_key.slice(2), "hex")
+    );
+    const epoch_id = ethers.getBytes(
+      Buffer.from(test_data.epoch_id.slice(2), "hex")
+    );
+    const sigma = ethers.getBytes(Buffer.from(test_data.sigma.slice(2), "hex"));
+    var enc_result;
+    try {
+      enc_result = await shutter.encrypt(msg, eonkey, epoch_id, sigma);
+    } catch (error) {
+      expect(error).toBeNull();
+    }
+    enc_result = ethers.hexlify(enc_result);
+    expect(enc_result).toEqual(test_data.expected);
+  });
+  test.each(decryption_tests)("%s", async (name, test_data) => {
+    const cipher = ethers.getBytes(
+      Buffer.from(test_data.cipher.slice(2), "hex")
+    );
+    const decryption_key = ethers.getBytes(
+      Buffer.from(test_data.epoch_secret_key.slice(2), "hex")
+    );
+    var dec_result;
+    try {
+      dec_result = await shutter.decrypt(cipher, decryption_key);
+    } catch (error) {
+      if (test_data.expected != "0x") {
+        expect(error).toBeNull();
+      } else {
+        return;
+      }
+    }
+    dec_result = ethers.hexlify(dec_result);
+    expect(dec_result).toEqual(test_data.expected);
+  });
+  test.each(verification_tests)("%s", async (name, test_data) => {
+    const epoch_secret = ethers.getBytes(
+      Buffer.from(test_data.epoch_secret_key.slice(2), "hex")
+    );
+    const eonkey = ethers.getBytes(
+      Buffer.from(test_data.eon_public_key.slice(2), "hex")
+    );
+    const epoch_id = ethers.getBytes(
+      Buffer.from(test_data.epoch_id.slice(2), "hex")
+    );
+    var verification;
+    try {
+      verification = await shutter.verifyDecryptionKey(
+        epoch_secret,
+        eonkey,
+        epoch_id
+      );
+    } catch (error) {
+      expect(error).toBeNull();
+    }
+    expect(verification == test_data.expected);
   });
 });
