571c9e93ee50725f2a3267b2e48b13a488e941b1
Jannik Luhn <jannik.luhn@posteo.de>
2024-06-24 11:45:28 +0200

    Add recent blocks table
    The table will be used in the future to facilitate reorg handling.

--- rolling-shutter/chainobserver/db/keyper/sql/schemas/keyper.sql
+++ rolling-shutter/chainobserver/db/keyper/sql/schemas/keyper.sql
@@ -5,3 +5,12 @@ CREATE TABLE keyper_set(
        threshold integer NOT NULL,
        PRIMARY KEY (keyper_config_index)
 );
+
+CREATE TABLE recent_block (
+       block_hash bytea NOT NULL,
+       block_number bigint NOT NULL,
+       parent_hash bytea NOT NULL,
+       timestamp bigint NOT NULL,
+       header bytea NOT NULL,
+       PRIMARY KEY (block_number)
+);
