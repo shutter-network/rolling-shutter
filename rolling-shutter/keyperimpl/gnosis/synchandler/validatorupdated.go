64fa579b3cfdb7132cb5e1e1592a824b3a8663cc
Maximilian Langenfeld <15726643+ezdac@users.noreply.github.com>
2024-10-11 13:28:28 -0400

    chore: re-enable logging in ValidatorUpdated handler

--- rolling-shutter/keyperimpl/gnosis/synchandler/validatorupdated.go
+++ rolling-shutter/keyperimpl/gnosis/synchandler/validatorupdated.go
@@ -7,6 +7,8 @@ import (
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	blst "github.com/supranational/blst/bindings/go"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
@@ -66,37 +68,39 @@ type ValidatorUpdated struct {
 	chainID uint64
 }
 
-func (kb *ValidatorUpdated) Address() common.Address {
-	return kb.address
+func (vu *ValidatorUpdated) Address() common.Address {
+	return vu.address
 }
 
-func (_ *ValidatorUpdated) Event() string {
+func (*ValidatorUpdated) Event() string {
 	return "Updated"
 }
 
-func (kb *ValidatorUpdated) ABI() abi.ABI {
-	return *kb.evABI
+func (vu *ValidatorUpdated) ABI() abi.ABI {
+	return *vu.evABI
 }
-func (kb *ValidatorUpdated) Accept(
+
+func (vu *ValidatorUpdated) Accept(
 	ctx context.Context,
 	header types.Header,
 	event bindings.ValidatorregistryUpdated,
 ) (bool, error) {
 	return true, nil
 }
+
 func (vu *ValidatorUpdated) Handle(
 	ctx context.Context,
 	update syncer.ChainUpdateContext,
 	events []bindings.ValidatorregistryUpdated,
 ) error {
 	db := database.New(vu.dbPool)
-	events, err := vu.filterEvents(ctx, events)
+	filteredEvents, err := vu.filterEvents(ctx, events)
 	if err != nil {
 		return err
 	}
 	err = vu.dbPool.BeginFunc(ctx, func(tx pgx.Tx) error {
 		dtbs := database.New(tx)
-		for _, event := range events {
+		for _, event := range filteredEvents {
 			msg := new(validatorregistry.RegistrationMessage)
 			err := msg.Unmarshal(event.Message)
 			if err != nil {
@@ -117,7 +121,7 @@ func (vu *ValidatorUpdated) Handle(
 		}
 		err = db.SetValidatorRegistrationsSyncedUntil(ctx, database.SetValidatorRegistrationsSyncedUntilParams{
 			//TODO: check int64 overflow
-			BlockNumber: int64(update.Append.Latest().Number.Int64()),
+			BlockNumber: update.Append.Latest().Number.Int64(),
 			BlockHash:   update.Append.Latest().Hash().Bytes(),
 		})
 		if err != nil {
@@ -132,15 +136,14 @@ func (vu *ValidatorUpdated) Handle(
 	if err != nil {
 		return errors.Wrap(err, "failed to get number of validator registrations")
 	}
-	// TODO: use the old zerolog again
-	// vu.log.Info(
-	// 	"synced validator registry",
-	// 	"start-block", qCtx.Update.Earliest(),
-	// 	"end-block", qCtx.Update.Latest(),
-	// 	"num-inserted-events", len(filteredEvents),
-	// 	"num-discarded-events", len(events)-len(filteredEvents),
-	// 	"num-registrations", numRegistrations,
-	// )
+	log.Info().
+		Int64("start-block", update.Append.Earliest().Number.Int64()).
+		Int64("end-block", update.Append.Latest().Number.Int64()).
+		Int("num-inserted-events", len(filteredEvents)).
+		Int("num-discarded-events", len(events)-len(filteredEvents)).
+		Int64("num-registrations", numRegistrations).
+		Msg("synced validator registry")
+
 	metrics.NumValidatorRegistrations.Set(float64(numRegistrations))
 	metrics.ValidatorRegistrationsSyncedUntil.Set(float64(update.Append.Latest().Number.Uint64()))
 	return nil
@@ -153,24 +156,23 @@ func (vu *ValidatorUpdated) filterEvents(
 	db := database.New(vu.dbPool)
 	filteredEvents := []bindings.ValidatorregistryUpdated{}
 	for _, event := range events {
-		//TODO:
-		// evLog := log.With().
-		// 	Hex("block-hash", event.Raw.BlockHash.Bytes()).
-		// 	Uint64("block-number", event.Raw.BlockNumber).
-		// 	Uint("tx-index", event.Raw.TxIndex).
-		// 	Uint("log-index", event.Raw.Index).
-		// 	Logger()
+		logger := log.With().
+			Hex("block-hash", event.Raw.BlockHash.Bytes()).
+			Uint64("block-number", event.Raw.BlockNumber).
+			Uint("tx-index", event.Raw.TxIndex).
+			Uint("log-index", event.Raw.Index).
+			Logger()
 
 		msg := new(validatorregistry.RegistrationMessage)
 		err := msg.Unmarshal(event.Message)
 		if err != nil {
-			//TODO:
-			// evLog.Warn().
-			// 	Err(err).
-			// 	Msg("failed to unmarshal registration message")
+			logger.Warn().
+				Err(err).
+				Msg("failed to unmarshal registration message")
 			continue
 		}
-		if !vu.checkStaticRegistrationMessageFields(msg, event.Raw.Address, msg.ValidatorIndex) {
+
+		if !vu.checkStaticRegistrationMessageFields(msg, event.Raw.Address, logger) {
 			continue
 		}
 
@@ -187,10 +189,10 @@ func (vu *ValidatorUpdated) filterEvents(
 			latestNonce = -1
 		}
 		if msg.Nonce > math.MaxInt64 || int64(msg.Nonce) <= latestNonce {
-			// evLog.Warn().
-			// 	Uint64("nonce", msg.Nonce).
-			// 	Int64("latest-nonce", latestNonce).
-			// 	Msg("ignoring registration message with invalid nonce")
+			logger.Warn().
+				Uint64("nonce", msg.Nonce).
+				Int64("latest-nonce", latestNonce).
+				Msg("ignoring registration message with invalid nonce")
 			continue
 		}
 
@@ -199,7 +201,7 @@ func (vu *ValidatorUpdated) filterEvents(
 			return nil, errors.Wrapf(err, "failed to get validator %d", msg.ValidatorIndex)
 		}
 		if validator == nil {
-			// evLog.Warn().Msg("ignoring registration message for unknown validator")
+			logger.Warn().Msg("ignoring registration message for unknown validator")
 			continue
 		}
 		pubkey, err := validator.Data.Validator.GetPubkey()
@@ -208,12 +210,12 @@ func (vu *ValidatorUpdated) filterEvents(
 		}
 		sig := new(blst.P2Affine).Uncompress(event.Signature)
 		if sig == nil {
-			// evLog.Warn().Msg("ignoring registration message with undecodable signature")
+			logger.Warn().Msg("ignoring registration message with undecodable signature")
 			continue
 		}
 		validSignature := validatorregistry.VerifySignature(sig, pubkey, msg)
 		if !validSignature {
-			// evLog.Warn().Msg("ignoring registration message with invalid signature")
+			logger.Warn().Msg("ignoring registration message with invalid signature")
 			continue
 		}
 
@@ -225,39 +227,34 @@ func (vu *ValidatorUpdated) filterEvents(
 func (vu *ValidatorUpdated) checkStaticRegistrationMessageFields(
 	msg *validatorregistry.RegistrationMessage,
 	validatorRegistryAddress common.Address,
-	validatorIndex uint64,
+	logger zerolog.Logger,
 ) bool {
-
-	// TODO: was passed in from outside
-	// logger := evLog.With().Uint64("validator-index", msg.ValidatorIndex).Logger()
+	logger = logger.With().Uint64("validator-index", msg.ValidatorIndex).Logger()
 	if msg.Version != ValidatorRegistrationMessageVersion {
-		// logger.Warn().
-		// 	Uint8("version", msg.Version).
-		// 	Uint8("expected-version", ValidatorRegistrationMessageVersion).
-		// 	Uint64("validator-index", msg.ValidatorIndex).
-		// 	Msg("ignoring registration message with invalid version")
+		logger.Warn().
+			Uint8("version", msg.Version).
+			Uint8("expected-version", ValidatorRegistrationMessageVersion).
+			Msg("ignoring registration message with invalid version")
 		return false
 	}
 	if msg.ChainID != vu.chainID {
-		// logger.Warn().
-		// 	Uint64("chain-id", msg.ChainID).
-		// 	Uint64("expected-chain-id", chainID).
-		// 	Uint64("validator-index", msg.ValidatorIndex).
-		// 	Msg("ignoring registration message with invalid chain ID")
+		logger.Warn().
+			Uint64("chain-id", msg.ChainID).
+			Uint64("expected-chain-id", vu.chainID).
+			Uint64("validator-index", msg.ValidatorIndex).
+			Msg("ignoring registration message with invalid chain ID")
 		return false
 	}
 	if msg.ValidatorRegistryAddress != validatorRegistryAddress {
-		// logger.Warn().
-		// 	Hex("validator-registry-address", msg.ValidatorRegistryAddress.Bytes()).
-		// 	Hex("expected-validator-registry-address", validatorRegistryAddress.Bytes()).
-		// 	Uint64("validator-index", msg.ValidatorIndex).
-		// 	Msg("ignoring registration message with invalid validator registry address")
+		logger.Warn().
+			Hex("validator-registry-address", msg.ValidatorRegistryAddress.Bytes()).
+			Hex("expected-validator-registry-address", validatorRegistryAddress.Bytes()).
+			Msg("ignoring registration message with invalid validator registry address")
 		return false
 	}
 	if msg.ValidatorIndex > math.MaxInt64 {
-		// logger.Warn().
-		// 	Uint64("validator-index", msg.ValidatorIndex).
-		// 	Msg("ignoring registration message with invalid validator index")
+		logger.Warn().
+			Msg("ignoring registration message with invalid validator index")
 		return false
 	}
 	return true
