27f66306caf6604a06a89cbed611834447617779
Konrad Feldmeier <konrad@brainbot.com>
2024-10-14 18:37:09 +0200

    Better debugging for unparseable data

--- rolling-shutter/p2p/floodsubpeerdiscovery/decodefloodsub_test.go
+++ rolling-shutter/p2p/floodsubpeerdiscovery/decodefloodsub_test.go
@@ -17,7 +17,7 @@ func (p *Peer) Print() error {
 	for _, addr := range p.Addrs {
 		ma, err := multiaddr.NewMultiaddrBytes(addr)
 		if err != nil {
-			return fmt.Errorf("could not parse multiaddr %e", err)
+			return fmt.Errorf("could not parse multiaddr %v: %e", string(addr), err)
 		}
 		fmt.Println(ma.String())
 	}
@@ -28,7 +28,7 @@ func (p *Peer) Print() error {
 
 func TestDecodeConst(t *testing.T) {
 	raw := "CiD53zJL+X7bj6vatGWpsYWmCBSsCTBi0jqBsHyc9yeM3xIIBKdjseMGWd0SCwSnY7HjkQJZ3c0D"
-	p, err := decodeFromB64(raw)
+	p, _, err := decodeFromB64(raw)
 	assert.NilError(t, err, "could not decode")
 	assert.Check(t, len(p.PublicKey) > 0, "no pubkey decoded")
 	assert.Check(t, len(p.Addrs) > 0, "no addresses decoded")
@@ -47,21 +47,23 @@ func TestDecodeMany(t *testing.T) {
 	assert.NilError(t, err, "could not open file")
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		p, err := decodeFromB64(scanner.Text())
+		p, raw, err := decodeFromB64(scanner.Text())
 		assert.NilError(t, err, "could not decode")
 		err = p.Print()
-		assert.NilError(t, err, "could not print")
+		if err != nil {
+			fmt.Printf("could not parse %v\n", raw)
+		}
 	}
 }
 
-func decodeFromB64(raw string) (Peer, error) {
+func decodeFromB64(raw string) (Peer, string, error) {
 	p := Peer{}
 	msg, err := base64.StdEncoding.DecodeString(raw)
 	if err != nil {
-		return p, err
+		return p, raw, err
 	}
 	proto.Unmarshal(msg, &p)
-	return p, nil
+	return p, raw, nil
 }
 
 func TestEncode(t *testing.T) {
