a912f352deb5014efe443ca269e65ddc72a95000
Konrad Feldmeier <konrad@brainbot.com>
2024-10-15 14:37:15 +0200

    Encode sender in output

--- rolling-shutter/p2p/floodsubpeerdiscovery/decodefloodsub_test.go
+++ rolling-shutter/p2p/floodsubpeerdiscovery/decodefloodsub_test.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/multiformats/go-multiaddr"
@@ -32,11 +33,11 @@ func (p *Peer) Print() error {
 		multiaddr.AddProtocol(DebugProtocol1)
 		ma, err := multiaddr.NewMultiaddrBytes(addr)
 		if err != nil {
-			return fmt.Errorf("could not parse multiaddr %v: %e", string(addr), err)
+			return fmt.Errorf("could not parse multiaddr %v: %v", string(addr), err)
 		}
 		ip, err := manet.ToIP(ma)
 		if err != nil {
-			return fmt.Errorf("could not extract ip %v: %e", string(addr), err)
+			return fmt.Errorf("could not extract ip %v: %v", string(addr), err)
 		}
 		name := knownIps["ip-to-name"][ip.String()]
 		id := knownIps["name-to-id"][name]
@@ -62,18 +63,19 @@ func TestParse(t *testing.T) {
 }
 
 func TestDecodeConst(t *testing.T) {
-	raw := "CiD53zJL+X7bj6vatGWpsYWmCBSsCTBi0jqBsHyc9yeM3xIIBKdjseMGWd0SCwSnY7HjkQJZ3c0D"
-	p, _, _, err := decodeFromB64(raw)
+	raw := "CiD53zJL+X7bj6vatGWpsYWmCBSsCTBi0jqBsHyc9yeM3xIIBKdjseMGWd0SCwSnY7HjkQJZ3c0D something"
+	p, from, _, _, err := decodeFromB64(raw)
 	assert.NilError(t, err, "could not decode")
 	assert.Check(t, len(p.PublicKey) > 0, "no pubkey decoded")
 	assert.Check(t, len(p.Addrs) > 0, "no addresses decoded")
+	assert.Check(t, len(from) > 0, "no addresses decoded")
 	err = p.Print()
 	assert.NilError(t, err, "could not print")
 }
 
-// extract msg data from nethermind logfile download (as $LOGFILE) via
+// extract msg data from nethermind logfile download JSON format (as $LOGFILE) via
 //
-// grep _peer-disc $LOGFILE|cut -b 119-|jq -r 'select(.publish != null)|.publish.[] | select (.topic=="_peer-discovery._p2p._pubsub")|.data' > /tmp/allmsgs.txt
+// cat $LOGFILE|jq -r '.[].line[94:]|fromjson?|select(.publish != null)|.publish.[] | select (.topic=="_peer-discovery._p2p._pubsub")| "\(.data) \(.from)"' > /tmp/allmsgs.txt
 //
 // before running this test
 func TestDecodeMany(t *testing.T) {
@@ -82,25 +84,31 @@ func TestDecodeMany(t *testing.T) {
 	assert.NilError(t, err, "could not open file")
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		p, raw, msg, err := decodeFromB64(scanner.Text())
+		p, from, raw, msg, err := decodeFromB64(scanner.Text())
 		assert.NilError(t, err, "could not decode")
 		err = p.Print()
 		if err != nil {
 			assert.Check(t, raw != "", "raw empty")
 			assert.Check(t, msg != nil, "msg empty")
-			fmt.Printf("could not parse %v: %v : %v : %v\n", raw, msg, p, err)
+			fmt.Printf("could not parse from %v %v: %v : %v : %v\n", from, raw, msg, p, err)
 		}
 	}
 }
 
-func decodeFromB64(raw string) (Peer, string, []byte, error) {
+func decodeFromB64(raw string) (Peer, string, string, []byte, error) {
 	p := Peer{}
-	msg, err := base64.StdEncoding.DecodeString(raw)
+	words := strings.Fields(raw)
+	if len(words) < 2 {
+		return p, "", raw, nil, fmt.Errorf("input malformed: %v", raw)
+	}
+	data := words[0]
+	from := words[1]
+	msg, err := base64.StdEncoding.DecodeString(data)
 	if err != nil {
-		return p, raw, msg, err
+		return p, from, raw, msg, err
 	}
 	proto.Unmarshal(msg, &p)
-	return p, raw, msg, nil
+	return p, from, raw, msg, nil
 }
 
 func TestEncode(t *testing.T) {
