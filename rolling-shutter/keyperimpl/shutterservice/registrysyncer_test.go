5ce99839cfe859360dc58d4e28c367a113cac014
faheelsattar <faheelsattar@yahoo.com>
2024-12-09 21:47:28 +0500

    add insert test

--- rolling-shutter/keyperimpl/shutterservice/registrysyncer_test.go
+++ rolling-shutter/keyperimpl/shutterservice/registrysyncer_test.go
@@ -1,6 +1,7 @@
 package shutterservice
 
 import (
+	"context"
 	"crypto/ecdsa"
 	"math/rand"
 	"testing"
@@ -9,7 +10,10 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/jackc/pgx/v4"
 	registryBindings "github.com/shutter-network/contracts/v2/bindings/shutterregistry"
+	"github.com/shutter-network/rolling-shutter/rolling-shutter/keyperimpl/shutterservice/database"
+	"github.com/shutter-network/rolling-shutter/rolling-shutter/medley/testsetup"
 	"gotest.tools/assert"
 )
 
@@ -35,6 +39,38 @@ func TestFilterIdentityRegisteredEvents(t *testing.T) {
 
 	finalEvents := rs.filterEvents(events)
 	assert.DeepEqual(t, finalEvents, events)
+}
+
+func TestInsertIdentityRegisteredEvents(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	ctx := context.Background()
+	events := make([]*registryBindings.ShutterregistryIdentityRegistered, 2)
+	for i := 0; i < 2; i++ {
+		identityPrefix, err := generateRandomBytes(32)
+		assert.NilError(t, err)
+		sender, err := generateRandomAddress()
+		assert.NilError(t, err)
+		events[i] = &registryBindings.ShutterregistryIdentityRegistered{
+			Eon:            uint64(i),
+			IdentityPrefix: [32]byte(identityPrefix),
+			Sender:         sender,
+			Timestamp:      rand.Uint64(),
+		}
+	}
+
+	dbpool, dbclose := testsetup.NewTestDBPool(ctx, t, database.Definition)
+	t.Cleanup(dbclose)
+
+	rs := RegistrySyncer{}
+
+	err := dbpool.BeginFunc(ctx, func(tx pgx.Tx) error {
+		err := rs.insertIdentityRegisteredEvents(ctx, tx, events)
+		return err
+	})
+	assert.NilError(t, err)
 }
 
 func generateRandomBytes(n int) ([]byte, error) {
