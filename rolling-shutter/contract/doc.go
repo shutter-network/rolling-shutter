452eef0dca724bc130b8eb0de1178ae74c0ffe88
blockchainluffy <jainpunit1997@gmail.com>
2025-04-08 11:55:25 +0530

    add abigen to generate go bindings

--- rolling-shutter/contract/doc.go
+++ rolling-shutter/contract/doc.go
@@ -1,4 +1,4 @@
 // package contract contains the abigen generated bindings for rolling shutter's smart contracts
 package contract
 
-// disabled //go:generate node ../../contracts/scripts/abigen.js
+//go:generate node ../../contracts/scripts/abigen.js
