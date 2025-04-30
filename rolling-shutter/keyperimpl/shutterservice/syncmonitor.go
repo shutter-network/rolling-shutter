abba24bb1cb343eebd6e3a1ba1428f4928c95412
blockchainluffy <jainpunit1997@gmail.com>
2025-04-30 15:53:27 +0530

    fix lint error

--- rolling-shutter/keyperimpl/shutterservice/syncmonitor.go
+++ rolling-shutter/keyperimpl/shutterservice/syncmonitor.go
@@ -31,7 +31,6 @@ func (s *SyncMonitor) Start(ctx context.Context, runner service.Runner) error {
 		for range ticker.C {
 			ctx.Done()
 			panic("explicitly canceled context error")
-			return fmt.Errorf("explicitly canceled context error")
 		}
 		return nil
 	})
