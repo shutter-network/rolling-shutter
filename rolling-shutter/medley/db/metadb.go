fa12275c7f68f9a4535d760a2917846b8e4b2370
blockchainluffy <47202792+blockchainluffy@users.noreply.github.com>
2025-05-28 15:58:04 +0530

    Update rolling-shutter/medley/db/metadb.go
    Co-authored-by: jannikluhn <jannik@brainbot.com>

--- rolling-shutter/medley/db/metadb.go
+++ rolling-shutter/medley/db/metadb.go
@@ -68,7 +68,7 @@ func ValidateSchemaVersion(ctx context.Context, tx pgx.Tx, definitionName string
 		return errors.Wrapf(ErrNeedsMigration, "expected version %d, have %d", version, haveVersion)
 	}
 	if int(haveVersion) != version {
-		return errors.Wrapf(ErrValueMismatch, "expected %d, have %d", version, haveVersion)
+		return errors.Wrapf(ErrValueMismatch, "expected version %d, have %d", version, haveVersion)
 	}
 	return nil
 }
