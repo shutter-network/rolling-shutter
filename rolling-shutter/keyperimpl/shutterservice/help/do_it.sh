b62aa1968c4b9100e31d244d428be5d6a86fa6df
Konrad Feldmeier <konrad@brainbot.com>
2025-07-16 15:27:29 +0200

    Add initial registry contract

--- rolling-shutter/keyperimpl/shutterservice/help/do_it.sh
+++ rolling-shutter/keyperimpl/shutterservice/help/do_it.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
-solc --combined-json abi,asm,ast,bin,bin-runtime,compact-format,devdoc,function-debug,function-debug-runtime,generated-sources,generated-sources-runtime,hashes,interface,metadata,opcodes,srcmap,srcmap-runtime,storage-layout,userdoc erc20.sol >erc20.combined.json
+solc --combined-json abi,asm,ast,bin,bin-runtime,devdoc,function-debug,function-debug-runtime,generated-sources,generated-sources-runtime,hashes,metadata,opcodes,srcmap,srcmap-runtime,storage-layout,userdoc erc20.sol >erc20.combined.json
 
 abigen --combined-json erc20.combined.json --pkg help --out erc20bindings.go
+
+solc --combined-json abi,asm,ast,bin,bin-runtime,devdoc,function-debug,function-debug-runtime,generated-sources,generated-sources-runtime,hashes,metadata,opcodes,srcmap,srcmap-runtime,storage-layout,userdoc ShutterEventTrigger.sol >eventtrigger.combined.json
+
+abigen --combined-json eventtrigger.combined.json --pkg help --out eventtriggerbindings.go
