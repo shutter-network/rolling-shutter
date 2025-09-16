a2cc7897769cd936e2d147e382a4d5206f99a420
blockchainluffy <jainpunit1997@gmail.com>
2025-09-16 16:56:40 +0530

    add explicit timeout for watch head

--- rolling-shutter/medley/chainsync/syncer/eonpubkey.go
+++ rolling-shutter/medley/chainsync/syncer/eonpubkey.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"time"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/log"
@@ -52,11 +51,9 @@ func (s *EonPubKeySyncer) Start(ctx context.Context, runner service.Runner) erro
 		}
 	}
 
-	x, cancel := context.WithTimeout(ctx, 2*time.Second)
-	defer cancel()
 	watchOpts := &bind.WatchOpts{
 		Start:   s.StartBlock.ToUInt64Ptr(),
-		Context: x,
+		Context: ctx,
 	}
 	s.keyBroadcastCh = make(chan *bindings.KeyBroadcastContractEonKeyBroadcast, channelSize)
 	subs, err := s.KeyBroadcast.WatchEonKeyBroadcast(watchOpts, s.keyBroadcastCh)
