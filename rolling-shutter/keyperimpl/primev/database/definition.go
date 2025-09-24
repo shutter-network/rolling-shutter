f0a55085b5194096a5966d335e07eb9cc334009b
blockchainluffy <jainpunit1997@gmail.com>
2025-09-24 17:23:51 +0530

    primev: resolve build after merging main

--- rolling-shutter/keyperimpl/primev/database/definition.go
+++ rolling-shutter/keyperimpl/primev/database/definition.go
@@ -17,7 +17,7 @@ var files embed.FS
 var Definition db.Definition
 
 func init() {
-	def, err := db.NewSQLCDefinition(files, "sql/", "primevkeyper", 1)
+	def, err := db.NewSQLCDefinition(files, "sql/", "primevkeyper")
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to initialize DB metadata")
 	}
