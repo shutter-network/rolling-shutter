79e454cd524a1d7f0282e44aaec10d8c365eaee2
Jannik Luhn <jannik.luhn@posteo.de>
2025-12-02 13:33:50 +0100

    fix: event based decryption trigger db migration
    The new tables for the event based decryption triggering were added to
    the schema file instead of to a migration. This works for new
    deployments, but not for existing ones. Now the new definitions have
    been moved to a migration file, fixing this issue.

--- rolling-shutter/keyperimpl/shutterservice/database/sql/schemas/shutterservice.sql
+++ rolling-shutter/keyperimpl/shutterservice/database/sql/schemas/shutterservice.sql
@@ -2,21 +2,6 @@
 -- Please change the version above if you make incompatible changes to
 -- the schema. We'll use this to check we're using the right schema.
 
-CREATE TABLE event_trigger_registered_event (
-    block_number bigint NOT NULL CHECK (block_number >= 0),
-    block_hash bytea NOT NULL,
-    tx_index bigint NOT NULL CHECK (tx_index >= 0),
-    log_index bigint NOT NULL CHECK (log_index >= 0),
-    eon bigint NOT NULL CHECK (eon >= 0),
-    identity_prefix bytea NOT NULL,
-    sender text NOT NULL,
-    definition bytea NOT NULL,
-    expiration_block_number bigint NOT NULL CHECK (expiration_block_number >= 0),
-    decrypted boolean NOT NULL DEFAULT false,
-    identity bytea NOT NULL,
-    PRIMARY KEY (eon, identity_prefix, sender)
-);
-
 CREATE TABLE identity_registered_event (
     block_number bigint NOT NULL CHECK (block_number >= 0),
     block_hash bytea NOT NULL,
@@ -50,22 +35,4 @@ CREATE TABLE decryption_signatures(
     identities_hash bytea NOT NULL,
     signature bytea NOT NULL,
     PRIMARY KEY (eon, keyper_index, identities_hash)
-);
-
-CREATE TABLE multi_event_sync_status (
-    enforce_one_row bool PRIMARY KEY DEFAULT true,
-    block_number bigint NOT NULL CHECK (block_number >= 0),
-    block_hash bytea NOT NULL
-);
-
-CREATE TABLE fired_triggers (
-    eon bigint NOT NULL,
-    identity_prefix bytea NOT NULL,
-    sender text NOT NULL,
-    block_number bigint NOT NULL CHECK (block_number >= 0),
-    block_hash bytea NOT NULL,
-    tx_index bigint NOT NULL CHECK (tx_index >= 0),
-    log_index bigint NOT NULL CHECK (log_index >= 0),
-    PRIMARY KEY (eon, identity_prefix, sender),
-    FOREIGN KEY (eon, identity_prefix, sender) REFERENCES event_trigger_registered_event (eon, identity_prefix, sender) ON DELETE CASCADE
 );
